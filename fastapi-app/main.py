3d6f30a1d5b2a57e06a09752d631b23b6de588e1
ooo <250376s@ugs.kochi-tech.ac.jp>
2024-08-11 00:16:05 +0900

    templateをextendsで分割した
    データベース処理をやろうとしてぶっ壊れてる

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 import sqlite3
 from pathlib import Path
+from db import init_db, getConnection # 自分で定義
 
 app = FastAPI()
-DB_PATH = Path("db.sqlite3")
-templates = Jinja2Templates(directory="templates/")
+templates = Jinja2Templates(directory="./templates/")
 
 @app.on_event("startup")
 async def startup():
+    # DB初期化
+    init_db()
     # データベースの接続を開く
-    app.state.db = sqlite3.connect(DB_PATH)
+    app.state.db = getConnection()
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -24,6 +26,35 @@ async def read_root(request: Request):
 #    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 #    tables = cursor.fetchall()
 #    return {"tables": tables}
-    context = {"request": request}
-    return templates.TemplateResponse("index.html", context)
+    return templates.TemplateResponse("top.html", {"request": request})
 
+@app.get('/list')
+async def showList(request: Request):
+    cursor = app.state.db.cursor()
+    items = cursor.execute('''
+        select * from item
+    '''
+    ).fetchall()
+    param = {'request': request, 'items': items}
+    return templates.TemplateResponse('list.html', param)
+    
+
+@app.get("/addRecord")
+async def addItem(request: Request):
+    logging.debug("addRecord")
+    cursor = app.state.db.cursor()
+    cursor.execute('''
+        insert into items (name, description) values
+        'test', 'this is a test')
+    ''')
+    return RedirectResponse(url='/', status_code=303)
+
+@app.get('/list')
+async def resetList(request: Request):
+    cursor = app.state.db.cursor()
+    items = cursor.execute('''
+        select * from item
+    '''
+    ).fetchall()
+    param = {'request': request, 'items': items}
+    return templates.TemplateResponse('list.html', param)
